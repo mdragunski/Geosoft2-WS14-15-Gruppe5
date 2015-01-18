60bb338039f7cfac32f6b8c9e8c769801a6f9ba9
JanVan01 <j_vanz01@uni-muenster.de>
2015-01-18 22:49:08 +0100

    add rudimentary search

--- public/modules/core/controllers/search.client.controller.js
+++ public/modules/core/controllers/search.client.controller.js
@@ -2,7 +2,61 @@
 
 angular.module('core').controller('SearchController', ['$scope',
 	function($scope) {
-		// Controller Logic
-		// ...
-	}
+		$scope.comments = [
+	{
+		'url':'google.de',
+		'creationdate':1421614549816,
+		'text':'blub',
+		'rating':3,
+		'timereference':1421614649816,
+		'georeference':'blub',
+		'tags':[
+		'bla',
+		'bla2',
+		'bla3'
+		],
+		'additionalressources':[
+		'blub'
+		],
+		'username':'anonymous'
+	},
+{
+	'url':'google.de',
+	'creationdate':1421614649816,
+	'text':'blub2 elefant',
+	'rating':4,
+	'timereference':1421614649816,
+	'georeference':'blub',
+	'tags':[
+	'bla',
+	'bla2',
+	'bla3'
+	],
+	'additionalressources':[
+	'blub'
+	],
+	'username':'anonymous'
+},
+{
+	'url':'google.de',
+	'creationdate':1421614649816,
+	'text':'blub hallo',
+	'rating':3,
+	'timereference':1421614649816,
+	'georeference':'blub',
+	'tags':[
+	'bla',
+	'bla2',
+	'bla3'
+	],
+	'additionalressources':[
+	'blub'
+	],
+	'username':'anonymous'
+}
+];
+
+
+
+}
 ]);
